cde791d0b042f16a8adfa4ae233c2f372d3e7f0f
suk-6 <me@suk.kr>
2024-07-18 12:29:42 +0900

    refactor: Update theme initialization function and add Pretendard font family

--- lib/theme.dart
+++ lib/theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData initThemeData({required Brightness brightness}) {
   if (brightness == Brightness.light) {
     return ThemeData(
+      fontFamily: "Pretendard",
       brightness: Brightness.light,
       colorScheme: const ColorScheme.light(
         primary: Color(0xFF5798F8),
@@ -11,6 +12,7 @@ ThemeData initThemeData({required Brightness brightness}) {
     );
   } else {
     return ThemeData(
+      fontFamily: "Pretendard",
       brightness: Brightness.dark,
       colorScheme: const ColorScheme.dark(
         primary: Color(0xFF5798F8),
